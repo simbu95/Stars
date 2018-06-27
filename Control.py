73487e92214016182c826700abc2fd809e21a936
simbu95 <simbu.tanksley@yahoo.com>
2018-06-27 00:24:00 -0500

    Add files via upload

--- Control.py
+++ Control.py
@@ -19,6 +19,11 @@ _SELECT_ARMY = actions.FUNCTIONS.select_army.id
 _NOT_QUEUED = [0]
 _SELECT_ALL = [0]
 
+#need to set these to what the actual map size is
+_Xsize=80
+_Ysize=80
+numpy.set_printoptions(threshold=numpy.inf)
+
 class SimpleAgent(base_agent.BaseAgent):
     def step(self, obs):
         super(SimpleAgent, self).step(obs)
@@ -29,6 +34,10 @@ class SimpleAgent(base_agent.BaseAgent):
           if not neutral_y.any() or not player_y.any():
             return actions.FunctionCall(_NO_OP, [])
           player = [int(player_x.mean()), int(player_y.mean())]
+          if obs.observation["game_loop"][0]%10==0:
+            print(player)
+#            test=State(obs)
+#            print(test.squares)   #Appears to be working
           closest, min_dist = None, None
           for p in zip(neutral_x, neutral_y):
             dist = numpy.linalg.norm(numpy.array(player) - numpy.array(p))
@@ -37,3 +46,20 @@ class SimpleAgent(base_agent.BaseAgent):
           return actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, closest])
         else:
           return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+
+
+class State(object):  #Need to add in state of player unit
+  def __init__(self,obs):
+    self.squares=numpy.zeros((_Xsize,_Ysize))
+    player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
+    neutral_y, neutral_x = (player_relative == _PLAYER_NEUTRAL).nonzero()
+    for p in zip(neutral_x, neutral_y):
+      self.squares[p[0],p[1]]=1
+
+
+# Defining the state is hard  ... Mostly done
+#but the task is easy
+# keep a list of all minerals  ... Done
+# move a marine to a mineral and remove the mineral ... Not Started
+# one state for every mineral a marine can move to 
+#with two marines, 2xminerals number of states. 
